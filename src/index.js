491a9fb5addca0ad77149d6d7a538e57f7a0c377
Mike van Eckendonk <mike.e@vecks.com.br>
2022-07-06 10:50:30 -0300

    add functionality CRUD

--- src/index.js
+++ src/index.js
@@ -1,39 +1,62 @@
-// eslint-disable-next-line no-unused-vars
 import './style.css';
+import ListTodo from './modules/list';
 
-const list = document.querySelector('.todo-items');
+const todos = new ListTodo();
 
-const myList = [
-  {
-    description: 'Need to wash my car',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Watch my favorite movie',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Go to the gym',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Complete todo app',
-    completed: false,
-    index: 4,
-  },
-];
+// constuctr todoList
+const renderList = () => {
+  let todoList = '';
+  todos.list.forEach((item) => {
+    todoList += `<li class="todo-li"><input type="checkbox">
+          <p contenteditable="true" class="todo-edit" id="${item.index}">${item.description}</p>
+          <i class="fas fa-trash icon trash"></i>
+          <i class="fas fa-ellipsis-v icon drag"></i>
+        </li>`;
+  });
+  document.querySelector('.todo-items').innerHTML = todoList;
 
-let todoList = '';
+  // add event listener to delete todo
+  const trash = document.querySelectorAll('.trash');
+  trash.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const index = e.target.parentNode.querySelector('.todo-edit').id;
+      todos.removeTodo(+index);
+      renderList();
+    });
+  });
 
-myList.forEach((item) => {
-  todoList += `<li class="todo-li"><input type="checkbox">
-    <p class="todo-edit" id="${item.index}">${item.description}</p>
-    <i class="fas fa-trash icon trash"></i>
-    <i class="fas fa-ellipsis-v icon drag"></i>
-    </li>`;
+  // add event listener to edit todo
+  const edit = document.querySelectorAll('.todo-edit');
+  edit.forEach((item) => {
+    item.addEventListener('blur', (e) => {
+      const index = e.target.id;
+      if (e.target.innerText.trim() === '') {
+        e.target.innerText = todos.list[+index - 1].description;
+        return;
+      }
+
+      const description = e.target.innerText;
+      const todo = {
+        description,
+        index,
+      };
+      todos.updateTodo(todo);
+      renderList();
+    });
+  });
+};
+
+// const myList = storageManager.getData();
+const input = document.querySelector('.input');
+input.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13 && input.value.trim() !== '') {
+    const newTodo = {
+      description: input.value,
+    };
+    input.value = '';
+    todos.addTodo(newTodo);
+    renderList();
+  }
 });
 
-list.innerHTML = todoList;
+renderList();
