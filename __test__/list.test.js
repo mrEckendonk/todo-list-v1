ce3c7ec3f3d2a40c54af078a7379c8970e31ba48
Mike van Eckendonk <mike.e@vecks.com.br>
2022-07-14 09:20:25 -0300

    Add test case Update & Complete

--- __test__/list.test.js
+++ __test__/list.test.js
@@ -15,3 +15,15 @@ describe('test Remove Function', () => {
     expect(todoList.list.length).toBe(0);
   });
 });
+
+describe('test Update Function', () => {
+  test('updateTodo', () => {
+    todoList.addTodo({ description: 'test' });
+    todoList.updateTodo({ description: 'test1', index: 1, completed: false });
+    expect(todoList.list[0].description).toBe('test1');
+  });
+  test('completed', () => {
+    todoList.updateTodo({ description: 'test1', index: 1, completed: true });
+    expect(todoList.list[0].completed).toBeTruthy();
+  });
+});
